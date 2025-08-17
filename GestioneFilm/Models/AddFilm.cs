6446b76fccc7be4ed41e22e4e4a5e5124221f6db
rober <rober@DESKTOP-F9MKTCD>
2025-08-17 09:57:51 +0200

    Aggiungere i file di progetto.

---
+++ GestioneFilm/Models/AddFilm.cs
@@ -0,0 +1,10 @@
+﻿namespace GestioneFilm.Models
+{
+    public class AddFilm
+    {
+        public string? Titolo { get; set; }
+        public string? Regista { get; set; }
+        public int Anno { get; set; }
+        public string? Genere { get; set; }
+    }
+}
---
+++ GestioneFilm/Models/Film.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestioneFilm.Models
+{
+    public class Film
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Titolo { get; set; }
+        public string? Regista { get; set; }
+        public int Anno { get; set; }
+        public string? Genere { get; set; }
+    }
+}
