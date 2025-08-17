baa4cf8f17d70bc765813e297b8356ec1f92fb04
Falco2101 <roberto.falconetti.business@gmail.com>
2025-08-17 10:23:54 +0200

    Reapply "Aggiungere i file di progetto."
    This reverts commit 446f953a357ec7da71688cb117b3d7e5e293a5ce.

---
+++ GestioneFilm/Database/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using GestioneFilm.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestioneFilm.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Film> Film { get; set; }
+    }
+}
